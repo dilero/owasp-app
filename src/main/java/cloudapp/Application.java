ae76ce24abb8d0d9cfc43109398f66e1fc4fcb7e
Cemile Diler Ozdemir (CT RDA DS EU TR DA MSA) <cemile.ozdemir@siemens.com>
2018-04-04 10:51:26 +0300

    initial commit

---
+++ src/main/java/cloudapp/Application.java
@@ -0,0 +1,14 @@
+package cloudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource({"classpath:application.properties", "classpath:application-db.properties"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
