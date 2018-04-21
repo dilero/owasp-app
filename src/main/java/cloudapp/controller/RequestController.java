78771fca0e1f27cc999fc1920b0729fbae0aa5a7
Diler <c.dilerozdemir@gmail.com>
2018-04-21 15:45:56 +0300

    download

--- src/main/java/cloudapp/controller/RequestController.java
+++ src/main/java/cloudapp/controller/RequestController.java
@@ -4,10 +4,20 @@ import cloudapp.entity.OperationType;
 import cloudapp.service.OperationService;
 import cloudapp.utils.OperationParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -22,6 +32,19 @@ public class RequestController {
             operationService.request(operationList, operationType);
         }
 
+    }
+
+    @GetMapping("/download")
+    public ResponseEntity<InputStreamResource> downloadFile1() throws IOException {
+
+        File file = new File("data.pdf");
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment;filename=" + file.getName())
+                .contentType(MediaType.APPLICATION_PDF).contentLength(file.length())
+                .body(resource);
     }
 
 
